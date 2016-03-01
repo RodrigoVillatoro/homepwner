e73a8f76b5a79429c890bdbae4752989dbb821b0
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2016-03-01 05:46:16 +0100

    Subclassing UITableViewCell

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Rodrigo Villatoro on 3/1/16.
+//  Copyright © 2016 Rodrigo Villatoro. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+}
